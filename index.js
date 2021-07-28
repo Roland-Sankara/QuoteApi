70aefb197008889a6fcd2fd1ab5eee94ceada9ec
Roland-Sankara <roland1sankara@gmail.com>
2021-07-28 12:14:06 +0300

    ft(add express router and mvc structure)

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// Import the Quote Model
-const Quote = require('./Models/Quote');
+// Routers
+const quoteRouter = require('./routes/quoteRouter');
 
 const app = express();
 
@@ -11,66 +11,8 @@ const app = express();
 // Middleware - helps modify the request
 app.use(express.json());
 
-
-
-// Routes of API
-app.get('/quotes',(req,res)=>{
-    // get all quotes from the DB
-    Quote.find()
-        .then((result)=>{
-            res.send(result)
-        })
-        .catch((error)=>{
-            res.send(error.message)
-        })
-
-})
-
-// Get a specific quote
-app.get('/quotes/:id',(req,res)=>{
-    Quote.findById(req.params.id)
-        .then((result)=>{
-            res.send(result)
-        })
-        .catch((error)=>{
-            res.send(error.message);
-        })
-    
-})
-
-// Post request - create a quote
-app.post('/quotes',(req,res)=>{
-   const quote = new Quote(req.body);
-   quote.save()
-    .then((result)=>{
-        res.send(result)
-    })
-    .catch((error)=>{
-        res.send(error.message)
-    })
-})
-
-// Update a quote
-app.patch('/quotes/:id',(req,res)=>{
-    Quote.findByIdAndUpdate(req.params.id,req.body)
-        .then((result)=>{
-            res.send(result)
-        })
-        .catch((error)=>{
-            res.send(error.message)
-        })
-})
-
-// Delete a quote
-app.delete('/quotes/:id',(req,res)=>{
-    Quote.findByIdAndDelete(req.params.id)
-        .then((result)=>{
-            res.send('Quote Deleted Successfully')
-        })
-        .catch((error)=>{
-            res.send(error.message)
-        })
-})
+// Routes for API
+app.use('/quotes',quoteRouter)
 
 
 
---
+++ routes/quoteRouter.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const quoteController = require('../controllers/quoteController');
+
+// Routes of API
+router.get('/', quoteController.getAllQuotes);
+
+// Get a specific quote
+router.get('/:id',quoteController.getQuoteById);
+
+// Post request - create a quote
+router.post('/', quoteController.createQuote);
+
+// Update a quote
+router.patch('/:id', quoteController.updateQuote)
+
+// Delete a quote
+router.delete('/:id', quoteController.deleteQuote)
+
+module.exports = router;
