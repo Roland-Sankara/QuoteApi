6edea45ecdb4faf56c74d0c0e37fbd3bf8b5eff6
HassanBahati <mukisabahati@gmail.com>
2021-07-29 13:33:56 +0300

    adjusted listen method

--- index.js
+++ index.js
@@ -24,14 +24,12 @@ const PORT = process.env.PORT || 3000;
 mongoose.connect(URL,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=>{
         console.log('Database Connection Established...');
-
-        // Start the Server
-        app.listen(PORT,()=>{
-            console.log('Server is listening on port 3000 ...')
-        })
     })
     .catch(()=>{
         console.log('Database Connection Failed !!')
     })
 
-
+// Start the Server
+app.listen(PORT,()=>{
+    console.log('Server is listening on port 3000 ...')
+})
