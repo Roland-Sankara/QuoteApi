83f4f2399e1a86568940d9645e4ea566e5b95630
HassanBahati <mukisabahati@gmail.com>
2021-08-11 11:39:12 +0300

    moved app listen method outside mongoose callback

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // Quote Route
-const quoteRoute = require('./routes/quoteRoute');
+const quoteRoute = require("./routes/quoteRoute");
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send("Welcome to quote app")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to quote app");
+});
 // Middleware - helps modify the request
 app.use(express.json());
 
 // Routes for API
-app.use('/quotes',quoteRoute)
-
+app.use("/quotes", quoteRoute);
 
 // Connect to Our Database
 const URL = process.env.DB_URL;
 const PORT = process.env.PORT || 3000;
-mongoose.connect(URL,{ useNewUrlParser: true, useUnifiedTopology: true })
-    .then(()=>{
-        console.log('Database Connection Established...');
-    })
-    .catch(()=>{
-        console.log('Database Connection Failed !!')
-    })
+mongoose
+  .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Database Connection Established...");
+  })
+  .catch(() => {
+    console.log("Database Connection Failed !!");
+  });
 
 // Start the Server
-app.listen(PORT,()=>{
-    console.log('Server is listening on port 3000 ...')
-})
+app.listen(PORT, () => {
+  console.log("Server is listening on port 3000 ...");
+});
