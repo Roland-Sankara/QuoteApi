d5a2295fab2b8fdc28a2d511cb092746491d1e9b
Roland-Sankara <roland1sankara@gmail.com>
2021-08-11 11:27:46 +0300

    ft(add initial server route)

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 
 // Middleware - helps modify the request
 app.use(express.json());
+
 //CORS
 app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
@@ -17,7 +18,12 @@ app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
 	next();
 });
+
 // Routes for API
+app.get('/',(req,res)=>{
+    res.send('Welcome to the Quote API')
+})
+
 app.use('/quotes',quoteRoute)
 
 
@@ -28,14 +34,14 @@ const PORT = process.env.PORT || 3000;
 mongoose.connect(URL,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=>{
         console.log('Database Connection Established...');
-
-        // Start the Server
-        app.listen(PORT,()=>{
-            console.log('Server is listening on port 3000 ...')
-        })
     })
     .catch(()=>{
         console.log('Database Connection Failed !!')
     })
+
+// Start the Server
+app.listen(PORT,()=>{
+    console.log('Server is listening on port 3000 ...')
+})
 
 
